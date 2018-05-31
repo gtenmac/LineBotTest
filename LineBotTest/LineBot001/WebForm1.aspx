c41e8a16901fccc7a854123c97a90f366a5bd623
陳書慕 <d757821137@gmail.com>
2018-05-31 15:15:16 +0800

    新增

---
+++ LineBotTest/LineBot001/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LineBot001.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Height="72px" OnClick="Button1_Click" Text="發送文字" Width="138px" />
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button2" runat="server" Height="75px" OnClick="Button2_Click" Text="發送貼圖" Width="137px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ LineBotTest/LineBot001/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LineBot001
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private isRock.LineBot.Bot bot = new isRock.LineBot.Bot("6h94g9LnHeVJ9LHD/b3bnwq1KNMiOXVeLbCtG/IjVNjnMi6zIXiDJl7+sF7NPoW+hbj5hp4pu+Fshh442TfM5QAa8nMhndeOLpLFPo/gxkQiFzrcZqyhEJqVp6hBsfNeg1uo5g+nyeJR+UrH08wyQAdB04t89/1O/w1cDnyilFU=");
+        private string UserID = "U99cad2819e3b6a3903ca2ee9882e58a0";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            bot.PushMessage(UserID, "testxxxxxxxxxx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            bot.PushMessage(UserID, 1, 3);
+        }
+    }
+}
